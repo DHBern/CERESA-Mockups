97cc1804a5a960856ecb0309e13eabfb998f3777
vvvyyynet <levyn.buerki@unibe.ch>
2025-10-31 18:28:52 +0100

    Add Eloisa

---
+++ src/routes/+page.svelte
@@ -0,0 +1,223 @@
+<script>
+	let fassung = $state('LF');
+
+	// <insSup>⌜[^⌝</insSup>]*⌝
+	//<insOver>[^</insOver>]*⌟
+</script>
+
+<button
+	onclick={() => {
+		fassung = fassung == 'DF' ? 'LF' : 'DF';
+	}}
+	class={['fixed top-0 left-0 rounded-xl p-2 px-5', fassung ? 'bg-red-200 font-bold' : '']}
+	>{fassung == 'DF' ? 'change to LF' : 'change to DF'}</button
+>
+
+<div data-fassung={fassung} class="m-20">
+	<h1 class="h1">Eloisa (frammento 2)</h1>
+	<p>
+		<handFree>
+			Eloisa<br />
+			o della predestinazione<br />
+		</handFree>
+	</p>
+	<p>
+		<strike>
+			Le stranezze di Eloisa incominciarono a manifestarsi in giovanissima<br />
+			età, sebbene in modo curioso.<br />
+			È forse il caso di sapere subito che
+		</strike> <strike>n</strike><hand>N</hand>ella <strike>sua</strike> famiglia materna
+		<insOver>di Eloisa</insOver><br />
+		vigeva da tempo immemorabile l’usanza di battezzare una figlia con<br />
+		il nome della madre, per cui le Eloise vi si susseguivano <strike>con regolarità</strike><insSup
+			>senza posa</insSup
+		><br />
+		salvo forse rarissimi casi di assenza di prole<insOver>,</insOver> o comunque prole fem<hyphen
+			>-</hyphen
+		><br />
+		minile<insOver>,</insOver> per una o l’altra di esse,<hand>;</hand> ma si raccontava che si era
+		allora<br />
+		provvedut<strike>o</strike>a ristabilire l’ordine con un piccolo scarto laterale
+		<insOver>,</insOver><br />
+		incaricando della bisogna una madre acquisita per <strike>intermissione</strike><insSup
+			>tramite</insSup
+		>
+		maschile<insOver>,</insOver><br />
+		<strike>pur sempre riferibile alla</strike><insSup>salvaguardando la</insSup> discendenza
+		diretta, per cui questi lievi<br />
+		disordini non avevano mai se non minimissimamente inquietato la tradi<hyphen>-</hyphen><br
+		/>zione, perseguita con <strike>rara</strike><insSup>lodevole</insSup> pertinacia. Nella
+		contingenza dell’attuale<br />
+		Eloisa, in casa erano presenti ben tre Eloise lei compresa, giusta<hyphen>-</hyphen><br />mente
+		situate una alle spalle dell’altra in quanto una madre e l’altra<br />
+		nonna della nostra. La cosa non sembrava disturbare nessuno salvo<br />
+		forse <strike>in</strike><insSup>nei</insSup> rari momenti in cui a un richiamo accorrevano ben
+		tre esempla<hyphen>-</hyphen><br />ri; ma essendo due di essi provvisti anche di appellativi
+		strettamente<br />
+		funzionali e assai più usati, non si pensò di ovviare magari con<br />
+		un nomignolo a un inconveniente che a nessuno sembrava tale.<br data-type="lf" />
+		Se non, come ben tosto apparve, all’ultima arrivata.<br data-type="lf" />
+		Rimangono celebri i tentativi intrapresi dalla nostra per uscire<br />
+		dal vicolo cieco di un’omonimia evidentemente per lei incomprensibile<br />
+		e perseguiti con testarda determinazione fin dal momento in cui incomin<hyphen>-</hyphen><br
+		/>ciò a dominare l’uso del linguaggio. Si ricordano le <strike>imperscrutabili</strike>
+		<insSup>preoccupanti</insSup><br />
+		ottusità opposte dalla medesima alle spiegazioni circa geografia<br />
+		e geometria dei legami parentali, per cui risultò impossibile tranquil<hyphen>-</hyphen><br
+		/>lizzare la piccola con la narrazione dell’usanza trasmessa di madre<br />
+		in figlia<insOver>,</insOver> o peggio ancora<insOver>,</insOver> nel nostro caso, di madre in
+		madre: dove<br />
+		appare dopotutto non troppo peregrina la manifesta difficoltà di<br />
+		figlia e nipote a vederne la logica concatenazione, <hand>;</hand>se non poi eviden<hyphen
+			>-</hyphen
+		><br />temente con l’andare del tempo. Seguirono <insSup>le</insSup> intemperanze di proteste<br
+		/>
+	</p>
+
+	<h1 class="h1">Eloisa (frammento 1)</h1>
+	<p>
+		In quegli anni Eloisa avevA comunque già diversi se non molti problemi<strike>.</strike><hand
+			>,</hand
+		><br />
+		visibili, questi, a occhio nudo.<br data-type="lf" />
+		Era graVEmente peggiorata a scuola, non diventando l’ultima della<br />
+		classe soltanto perché c’era dopo di lei chi effettivamente poteva<br />
+		mostrare qualche giustificabile motivazione al poco rendimento con<br />
+		innate e facilmente rintracciabili predisposizioni alla manifesta<strike>ta</strike><br />
+		<strike>poca presenza</strike> <insSup>pochezza</insSup> di cervello, come digià s’era visto
+		<insSup>notato</insSup>
+		ne<hand>a</hand>lle scuole elemen<hyphen /><br />tari <hand>poche</hand>frequentate qua e là a
+		<insSup><strike>tari frequantate </strike>tari frequentate da questo o quello a</insSup>
+		ripetizione: mentre la nostra fin lì<br />
+		<strike>
+			poteva mostrare <insSup>presentare<unclear>?</unclear></insSup> un addirittura brillante curriculum.
+		</strike><br />
+		aveva vantato un<insOver>a</insOver> addirittura brillante curriculum.
+		<insOver>carriera scolastica <insSub>di allieva.</insSub></insOver>
+		.<br data-type="lf" />
+		Si dedicava assiduamente a occupazioni frivole, quali la ginnastica<br />
+		serale in gruppi di volontarie e volontari ad alto rendimento fisico<br />
+		mentre non si sarebbe proprio potuto dire che disponesse lei stessa<br />
+		di particolari doti sportive, per quanto e non senza grazia si sfor<hyphen /><br />zasse; o
+		ancora la costituzione e l’animazione di compagnie filo<hyphen /><br />drammatiche di quartiere,
+		rimaste del resto sempre ignote ai più,<br />
+		ma senz’altro sporadicamente apprezzate in occasione di ricorrenti<br />
+		celebrazioni scolastiche ufficiali per il resto grandemente noiose<br />
+		sia per le scolaresche che per i genitori ufficialmente convocati:<br />
+		e qui un qualche successo lo riscuoteva, forse in virtù del lungo<hand>a</hand><insOver
+			>mente</insOver
+		> <br />
+		<strike>allenamento</strike> <insSup>esercitato talento</insSup> teatrali<hand>e</hand><strike
+			>zzante</strike
+		>
+		di cui come si sa già in passato aveva<br />
+		dato prova; e altre sciocchezze di questo genere, richiedenti però<br />
+		notevole dispendio di tempo <insSup>e energie</insSup> senza dubbio sottratto<hand>i</hand> a
+		più seriose<br />
+		e utili applicazioni,: il che <strike>senza dubbio</strike> <insSup>già</insSup> una qualche
+		complica<hyphen /><br />zione comportava perché la sua massima aspirazione in realtà era<br />
+		la lettura alla quale nottetempo si dedicava accanitamente per sfuggire<br />
+		al proprio presente che considerava se non squallido però del tutto<br />
+		insoddisfacente.<br data-type="lf" />
+	</p>
+	<blockContent data-label="Inserted text">
+		Intraprendeva perciò, e tutte quante sgranate lungo l’arco di una<br />
+		giornata, varie e diverse carriere, alle quali separatamente si dedi<hyphen /><br />cava con
+		trasporto e ampie immagi’<hand>n</hand>azioni proiettate in un favoloso<br />
+		futuro ogni volta poi con scoramento rimandante a un altro e un altro<br />
+		ancora, comportandosi tutti<hand>o</hand> sommato come un pippistrello accecato<br />
+		dalla luce fra pareti insormontabili, presa di volta in volta da<br />
+		inspiegabile sconforto, per fortuna di breve durata perché appunto<br />
+		scacciato dalla successiva immersione o emersione come poi si voglia.<br />
+		<strike>
+			E non in ultimo grandemente recalcitrava davanti alle vicende e alle<br />
+			pretese della propria e altrui realtà. <hand>,</hand>
+			<insSup>del proprio e altrui sesso,</insSup> <br />
+			di cui del resto, benché se ne capacitasse sebbene a modo suo, poco<br />
+			o nulla riusciva a capire. <hand>,</hand><br />
+			e tanto meno a accettare, appunto.
+		</strike><br />
+	</blockContent>
+	<p>
+		Ma <insSup>tutto</insSup> questo indubbiamente non avrebbe potuto bastare per costituire<br />
+		un serio problema, non fosse stato che in realtà essa grandemente<br />
+		recalcitrava davanti alle vicende e alle pretese del proprio e dell’<br />
+		altrui sesso, di cui, sebbene se ne capacitasse perlomeno a modo<br />
+		suo, poco o nulla riusciva a capire e tanto meno, <strike>a accettare, appunto.</strike>
+		<insOver>sembra di intendere, ad accettare.</insOver>
+	</p>
+</div>
+
+<style>
+	@reference "tailwindcss";
+
+	p {
+		@apply my-5;
+	}
+	h1,
+	h2,
+	h3,
+	h4 {
+		@apply text-2xl font-bold;
+	}
+
+	[data-fassung='LF'] {
+		insOver,
+		insSup,
+		strike,
+		br:not([data-type='lf']),
+		hyphen {
+			@apply hidden;
+		}
+	}
+
+	[data-fassung='DF'] {
+		blockContent::before {
+			@apply absolute -top-2 -left-2 bg-sky-100 px-2 py-1 font-bold text-black content-[attr(data-label)];
+		}
+
+		blockContent {
+			@apply relative block border-2 border-sky-600 p-4 pt-7;
+		}
+
+		hand,
+		handFree,
+		insOver,
+		insSup,
+		hand {
+			@apply text-blue-500 italic;
+		}
+
+		insSup {
+			@apply text-xs;
+			vertical-align: super;
+		}
+		strike insSup {
+			@apply line-through;
+		}
+		insOver::before {
+			content: '⌜';
+		}
+		insOver::after {
+			content: '⌟';
+		}
+		insSup::before {
+			content: '⌜';
+		}
+		insSup::after {
+			content: '⌝';
+		}
+		hand::before {
+			content: '{';
+		}
+		hand::after {
+			content: '}';
+		}
+		unclear::before {
+			content: '[';
+		}
+		unclear::after {
+			content: ']';
+		}
+	}
+</style>
