3a6305cbbefcce5045b6e3b06bd08a0407f63ad6
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-01 12:57:48 +0100

    feat: add anima

---
+++ src/routes/docs/anima/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<h1 class="h1">Anima</h1>
+<p>ANIMA</p>
+<p>
+	Secondo il dizionario, principio immateriale della vita negli<br />
+	esseri viventi; mentre, sempre secondo il dizionario, la<br />
+	<a data-crossref href={`${base}/docs/vita`}>VITA</a> risulta essere la forza attiva propria degli
+	esseri<br />
+	animali e vegetali in virtù della quale, oltre che muoversi,<br />
+	nutrirsi, assimilare e quant’altro, sono anche il grado di<br />
+	riprodursi sotto forme sempre uguali.<br />
+	Fra anima e vita corre dunque un rapporto di reciproca dipend-<br />
+	denza il quale tuttavia non risulta indispensabile alla vita<br />
+	né animale né vegetale né altra, in quanto l’ANIMA è alloggia-<br />
+	ta esclusivamente nella specie umana, sottospecie della fa-<br />
+	miglia dei mammiferi, eppure non tutti i mammiv<replOver><old>e</old><new>f</new></replOver>eri
+	dispongono<br />
+	di anima esattamente come non ne dispongono altri vertebrati,<br />
+	gli invertebrati, gli insetti, i microorganismi, i vegetali<br />
+	ecc.. Un cane, pur essendo mosso dal principio attivo della<br />
+	vita, può fare a meno del principio immateriale della stessa<br />
+	e pertanto non disporre di un’anima, così come non ne dispon-<br />
+	gono vermi, amebe o piante. Se ne deduce pertanto che l’ANIMA<br />
+	è un organismo non so<insInline>l</insInline>tanto invisibile, inodore, asonoro, im-<br />
+	palpabile e insipido, ma anche razzista.<br />
+	È nonidemo studiata da apposite discipline e amministrata<br />
+	da specifiche istituzioni quali filosofie e psicologie da una<br />
+	parte, chie<replOver><old>de</old><new>se</new></replOver> e manicomi dall’altra, per non citare
+	che le più<br />
+	appariscenti; e partecipa per estensione a tutte le attività<br />
+	umane, dalla riproduzione della specie alla guerra, nonché a<br />
+	quelle più squisitamente sociali, dalla politica ai tribunali,<br />
+	senza che le sia concesso dire bah.<br />
+	<br />
+	L’anima infatti, essendo immateriale, non può <strikeT>nemmeno mordere.</strikeT><br />
+	parlare e neanche <strikeT>mordere</strikeT> е tanto meno mordere.<br />
+	<br />
+	<strikeH>
+		Anche le donne hanno evidentemente dovuto vivere per millenni<br />
+		senza l’ANIMa, essendo questa <movedBlockRef data-ref="1" /> stata
+		<movedBlock data-ref="1" data-label="parola riposizionata">loro</movedBlock>
+		concessa soltanto<br />
+		nel per gentile concessione del Concilio
+	</strikeH><br />
+</p>
