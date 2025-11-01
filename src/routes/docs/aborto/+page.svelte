e6fc80047abe58e46600aaae80034f797b4bed96
vvvyyynet <levyn.buerki@unibe.ch>
2025-11-01 19:24:23 +0100

    feat: add second page of aborto

--- src/routes/docs/aborto/+page.svelte
+++ src/routes/docs/aborto/+page.svelte
@@ -11,44 +11,69 @@
 		<h1 class="h1">Aborto</h1>
 		<p>ABORTO (Liberalizzazione delete)</p>
 		<p>
-			Attiene alla particolare prassi riproduttiva dei mammiferi la quale, <lb />com’è noto,
-			discende dalla copulazione di due diversi organismi bio-<lb break="no" />logici, uno femminile
-			e l’altro maschile, o perlomeno così denomina-<lb break="no" />ti. Quali che siano le modalità
-			di questa copulazione, l’embrione
-			<lb />mammifero si sviluppa uterinamente, vale a dire all’interno dell’or-<lb
-				break="no"
-			/>gano fisiologico femminile attrezzato per questa bisogna, e l’organi-<lb break="no" />smo
-			femminile ne detiene quindi la proprietà di fatto. L’interruzione <lb />dello sviluppo
+			Attiene alla particolare prassi riproduttiva dei mammiferi la quale, <br />com’è noto,
+			discende dalla copulazione di due diversi organismi bio<hyphen>-</hyphen><br />logici, uno
+			femminile e l’altro maschile, o perlomeno così denomina<hyphen>-</hyphen><br />ti. Quali che
+			siano le modalità di questa copulazione, l’embrione
+			<br />mammifero si sviluppa uterinamente, vale a dire all’interno dell’or<hyphen>-</hyphen><br
+			/>gano fisiologico femminile attrezzato per questa bisogna, e l’organi<hyphen>-</hyphen><br
+			/>smo femminile ne detiene quindi la proprietà di fatto. L’interruzione <br />dello sviluppo
 			dell’embrione viene appunto chiamat<subst>
 				<delete>o</delete>
 				<add place="over">a</add>
 			</subst> ABORTO.
 		</p>
 		<p>
-			Molti mammiferi dispongono di un naturale controllo delle nascite in <lb />quanto la
-			ricettività fisiologica femminile avviene ciclicamente a <lb />intervalli notevolmente
-			distanziati, e in quanto l’organismo biologi-<lb break="no" />co femminile esercita il
-			richiamo delete partner della copulazione sol-<lb break="no" />tanto nel periodo della
-			ricettività. Nei mammiferi umani non esiste <lb />invece alcun controllo naturale delle
-			nascite e richiami e ricettivi-<lb break="no" />tà si esercitano indiscriminatamente, vale a
-			dire sempre e ovunque. <lb />Questo spiega perché debba intervenire una regolamentazione
-			artifi-<lb break="no" />ciale della riproduttività umana, affinché non ogni copulazione ab-<lb
-				break="no"
-			/>bia a determinare riproduzione provocando non soltanto sgradevolezze <lb />varie ma anche la
-			proliferazione incontrollata e incontrollabile delete-<lb break="no" />la specie.
+			Molti mammiferi dispongono di un naturale controllo delle nascite in <br />quanto la
+			ricettività fisiologica femminile avviene ciclicamente a <br />intervalli notevolmente
+			distanziati, e in quanto l’organismo biologi<hyphen>-</hyphen><br />co femminile esercita il
+			richiamo delete partner della copulazione sol<hyphen>-</hyphen><br />tanto nel periodo della
+			ricettività. Nei mammiferi umani non esiste <br />invece alcun controllo naturale delle
+			nascite e richiami e ricettivi<hyphen>-</hyphen><br />tà si esercitano indiscriminatamente,
+			vale a dire sempre e ovunque. <br />Questo spiega perché debba intervenire una
+			regolamentazione artifi<hyphen>-</hyphen><br />ciale della riproduttività umana, affinché non
+			ogni copulazione ab<hyphen>-</hyphen><br />bia a determinare riproduzione provocando non
+			soltanto sgradevolezze
+			<br />varie ma anche la proliferazione incontrollata e incontrollabile delete<hyphen>-</hyphen
+			><br />la specie.
 		</p>
 		<p>
-			Come ogni regolamentazione esulante dalle leggi della natura, anche <lb />quella della
-			riproduttività umana riveste necessariamente un carat-<lb break="no" />tere amministrativo in
-			quanto affidata alla legiferazione sociale. <lb />Appare logicamente corretto che, trovandoci
-			nella fattispecie di
-			<lb />fronte a una produzione, l’amministratività si sia esercitata sul <lb />fornitore
-			effettivo delete prodotto, vale a dire sull’organismo biolo-<lb break="no" />gico femminile,
-			nei riguardi delete quale si è per semplicità risaliti <lb />anche a monte ponendo limitazioni
-			alla permissività della copulazio-<lb break="no" />ne femminile perlomeno nella prassi, nonché
-			discesi a valle per
-			<lb />quanto appunto riguarda l’ABORTO. L’amministratività ha sempre bada-<lb break="no" />to
-			al sodo, non competendole compiti di maggiore sottigliezza.
+			Come ogni regolamentazione esulante dalle leggi della natura, anche <br />quella della
+			riproduttività umana riveste necessariamente un carat<hyphen>-</hyphen><br />tere
+			amministrativo in quanto affidata alla legiferazione sociale. <br />Appare logicamente
+			corretto che, trovandoci nella fattispecie di
+			<br />fronte a una produzione, l’amministratività si sia esercitata sul <br />fornitore
+			effettivo delete prodotto, vale a dire sull’organismo biolo<hyphen>-</hyphen><br />gico
+			femminile, nei riguardi delete quale si è per semplicità risaliti <br />anche a monte ponendo
+			limitazioni alla permissività della copulazio<hyphen>-</hyphen><br />ne femminile perlomeno
+			nella prassi, nonché discesi a valle per
+			<br />quanto appunto riguarda l’ABORTO. L’amministratività ha sempre bada<hyphen>-</hyphen><br
+			/>to al sodo, non competendole compiti di maggiore sottigliezza.
+		</p>
+		<p>2 (aborto)</p>
+		<p>
+			Così applicandosi a uno solo dei due contraenti della copulazione e<br />
+			privilegiando l’uno nei confronti dell’altro, lasciato di fatto to<hyphen>-</hyphen><br />
+			talmente in disparte, la regolamentazione riproduttiva ha in realtà <br />
+			del tutto correttamente recepito lo stato di maggiore importanza ri<hyphen>-</hyphen><br />
+			vestito nella questione dal produttore effettivo o detentore
+			<delete type="typewriter">del ma<illegible>[xx]</illegible></delete> <br />
+			materiale unico del prodotto in causa. Difficilmente infatti avrebbe <br />
+			potuto esercitarsi sul concorrente emarginato, il ruolo di costui es<hyphen>-</hyphen><br />
+			sendo puramente ausiliario e quindi trascurabile perlomeno in termini <br />
+			di economia di mercato.<br />
+			La circostanza tuttavia che regolamentazione e amministrazione siano <br />
+			state, contrariamente a ogni logica produttiva, assunte dal contraen<hyphen>-</hyphen><br />
+			te ausiliario e quindi materialisticamente
+			<subst
+				><delete type="typewriter">trascurabile</delete><add place="above" type="typewriter"
+					>irrilevante,</add
+				></subst
+			>
+			fa oggi l’og<hyphen>-</hyphen><br />
+			getto di una vertenza sindacale avanzata dal contraente amministrato,<br />
+			vertenza nota con il timido e alquanto improprio nome di “liberaliz<hyphen>-</hyphen><br />
+			zazione dell’ABORTO”.<br />
 		</p>
 	</div>
 </div>
